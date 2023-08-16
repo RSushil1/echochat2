bda30a2fb643280d3a970a4580789ace01577d62
RSushil1 <sushilsinghrathore1998@gmail.com>
2023-08-16 11:37:54 +0530

    ,

--- src/context/SocketProvider.jsx
+++ src/context/SocketProvider.jsx
@@ -3,28 +3,20 @@ import io from 'socket.io-client'
 import { UseAuth } from './Auth'
 const SocketContext = React.createContext()
 
-export function useSocket() {
-  return useContext(SocketContext)
+export function useSocket(){
+    return useContext(SocketContext)
 }
 
-const SocketProvider = ({ children }) => {
+const SocketProvider = ({children}) => {
   const [socket, setSocket] = useState();
   const [auth] = UseAuth();
   const id = auth?.user?._id
 
-  useEffect(() => {
-    const newSocket = io('https://echochatserver.vercel.app',
-      {
-        query: { id },
-        withCredentials: true,
-        extraHeaders: {
-          "my-custom-header": "abcd"
-        },
-        transports:['websocket', 'polling']
-      })
-    setSocket(newSocket)
-    return () => newSocket.close()
-  }, [id])
+    useEffect(()=>{
+        const newSocket = io('http://localhost:8000',{query:{id}})
+        setSocket(newSocket)
+        return ()=>newSocket.close()
+    },[id])
   return (
     <SocketContext.Provider value={socket}>
       {children}
